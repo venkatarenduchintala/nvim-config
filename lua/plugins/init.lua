6fcdc2aed219a0abf80c8dfa85e3818fd2e27c9a
Venkata Subramani Renduchintala <venkatasubramani@protonmail.ch>
2024-08-05 12:09:54 +0200

    Removed CoPilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -95,7 +95,7 @@ local plugins = {
 	-- require("plugins.configs.blink"),
 	require("plugins.configs.lspsignature"),
 
-  require("plugins.configs.copilot"),
+--  require("plugins.configs.copilot"),
 	-- Tmux
 	require("plugins.configs.tmux"),
 	---- Git
